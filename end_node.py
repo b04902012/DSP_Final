48357009da1ed7c1da828244168126445086c72d
zolution <zolution@outlook.com>
2018-01-14 22:29:15 +0800

    Fix comment syntax typo

--- end_node.py
+++ end_node.py
@@ -2,7 +2,7 @@ datatype = __import__('datatype')
 node = datatype.node
 graph = datatype.graph
 
-def end_node(g,s): //g = graph, s = node string
+def end_node(g,s): #g = graph, s = node string
     punctuation_list = [",", "!", ".", "?", ";"]
     if s.split('^')[0] in punctuation_list:
         return true
