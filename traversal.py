3bb42c8bd3cac411d394880af271167db7c265fe
albert0liu0 <albert0liu0@gmail.com>
2018-01-15 03:50:16 +0800

    threshold

--- traversal.py
+++ traversal.py
@@ -1,11 +1,12 @@
 start_node=__import__('start_node').start_node
 end_node=__import__('end_node').end_node
+redundancy=__import__('scoring').redundancy
 
 
 def traversal(g):
     path_list=[]
     def traval(g,s,pp):
-        if(len(pp)>10):
+        if(len(pp)>20):
             return
         p=pp[0:]
         if(end_node(g,s)):
@@ -14,7 +15,7 @@ def traversal(g):
             return
         for s2 in g.nodes[s]['next']:
             if(not g.get_visited(s2)):
-                if(redundancy(g,[s,s2])>len(g.nodes)//10):
+                if(redundancy(g,[s,s2])>len(g.nodes)//200):
                     g.visit(s2)
                     p.append(s)
                     traval(g,s2,p)
