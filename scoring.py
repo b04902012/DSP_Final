25adce1da8af258f26993d3ff9fa6044dda1e4e7
zolution <zolution@outlook.com>
2018-01-14 22:28:38 +0800

    Finish redundancy and simple scoring

---
+++ scoring.py
@@ -0,0 +1,23 @@
+datatype = __import__('datatype')
+graph = datatype.graph
+node = datatype.node
+
+def redundancy(g, p): #g = graph, p = path
+    intersect = [r[0] for r in g.get_pri(p[0])]
+    for i in p[1:]:
+        pri_list = [r[0] for r in g.get_pri(i)]
+        ite = intersect
+        for j in ite:
+            if j not in pri_list:
+                intersect.remove(j)
+    return len(intersect)
+
+def scoring_simple(g, p): #g = graph, p = path
+    length = len(p)
+    sum_of_redundancy = 0.0
+    for i in range(length):
+        for k in range(i+1, length):
+            sum_of_redundancy += redundancy(g, p[i:k+1])
+    return sum_of_redundancy / length
+
+
