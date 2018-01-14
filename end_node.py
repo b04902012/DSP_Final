1a42b653889af2da65dfbe823d7161ae52d143d0
zolution <zolution@outlook.com>
2018-01-14 21:54:34 +0800

    Finish end_node.py

---
+++ end_node.py
@@ -0,0 +1,13 @@
+datatype = __import__('datatype')
+node = datatype.node
+graph = datatype.graph
+
+def end_node(g,s): //g = graph, s = node string
+    aim = g.get_pri(s)
+    punctuation_list = [",", "!", ".", "?", ";"]
+    if s.split('^')[0] in punctuation_list:
+        return true
+    if s.split('^')[1] == "CC":
+        return true
+    return false
+
