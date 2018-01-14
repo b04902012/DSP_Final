4f3fa303a286cc1cf76d250e02d03971dbde6b37
zolution <zolution@outlook.com>
2018-01-14 21:43:00 +0800

    Make all character lowercase

--- POS_tagging.py
+++ POS_tagging.py
@@ -1,7 +1,7 @@
 import nltk
 
 def pos_tagging(input_string):
-    text = nltk.word_tokenize(input_string)
+    text = nltk.word_tokenize(input_string.lower())
     tagged_list = nltk.pos_tag(text)
     return ['^'.join(ite) for ite in tagged_list]
 
