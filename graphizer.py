0f5a0eb1a10ce5acd17c00df0c45f7a7ca419757
albert0liu0 <albert0liu0@gmail.com>
2018-01-15 04:00:26 +0800

    th

--- graphizer.py
+++ graphizer.py
@@ -1,14 +1,18 @@
 datatype=__import__('datatype')
 node=datatype.node
 graph=datatype.graph
-def graphize(sentences):
+def graphize(sentences,word_count,frequency_threshold):
     g=graph()
     for sid in range(0,len(sentences)):
         sentence=sentences[sid]
         for wid in range(0,len(sentence)):
             word=sentence[wid]
+            if(word_count[word]<frequency_threshold):
+                continue
             g.add_node(word)
             g.add_pri(word,(sid,wid))
             if(wid>0):
-                g.add_edge(sentence[wid-1],word)
+                if(word_count[sentence[wid-1]]>=frequency_threshold):
+                    g.add_edge(sentence[wid-1],word)
+        
     return g
--- main.py
+++ main.py
@@ -8,11 +8,21 @@ traversal=__import__('traversal').traversal
 scoring_simple=__import__('scoring').scoring_simple
 output=__import__('output').output
 
+frequency_threshold=5
+
 sentences=open(sys.argv[1],mode='rt').read().split('\n')[0:-1]
 tagged_sentences=[]
+
+word_count={}
 for i in range(0,len(sentences)):
     tagged_sentences.append(pos_tagging(sentences[i]))
-g=graphize(tagged_sentences)
+    for word in tagged_sentences[i]:
+        if(word not in word_count):
+            word_count[word]=0
+        word_count[word]=word_count[word]+1
+
+        
+g=graphize(tagged_sentences,word_count,frequency_threshold)
 
 
 path_list=traversal(g)
